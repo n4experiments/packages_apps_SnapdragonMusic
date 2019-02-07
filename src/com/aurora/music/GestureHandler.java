f7c837db18be65379f895803a6b908a9e4d886ea
Mr. Dragon <whyorean@gmail.com>
2019-02-07 17:28:35 +0530

    Update Copyrights
    /* Copyright (c) 2019, Aurora OSS, Last Modified 2/7/19 5:05 PM
     * Copyright (C) 2007-2018, The Android Open Source Project
     * Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
     *
     * ------------------------------------------------------------------
     * ------------------------------------------------------------------
     */

--- src/com/aurora/music/GestureHandler.java
+++ src/com/aurora/music/GestureHandler.java
@@ -1,18 +1,21 @@
-/*Copyright (c) 2014, The Linux Foundation. All rights reserved.
-
+/*
+ * Copyright (c) 2019, Aurora OSS, Last Modified 2/7/19 5:04 PM
+ * Copyright (C) 2007-2018, The Android Open Source Project
+ * Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
+ *    * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *    * Neither the name of The Linux Foundation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
